1098fbefc76f832e53b3862b747f9f3a1b69ae76
Shishuai Wang <shishuai@amazon.com>
2020-10-13 16:20:59 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,17 +1,41 @@
-## My Project
+# Amazon SageMaker Processing Video2frame Model Inference
 
-TODO: Fill this README out!
+This demo shows how to use SageMaker processing process video frames extraction and model inference.
 
-Be sure to:
+Some business scenario need to processing videos by using machine learning. They usually need extract frames from videos and then send them to models and get the result. This need you extract the frames and store in some place and then using batch transformer or online inference, which would involve a storage cost which is no longer need after inference. So customers are looking for a way to finish such job in a effective way, here we would introduce Amazon SageMaker Processing.
 
-* Change the title in this README
-* Edit your repository description on GitHub
+Amazon SageMaker Processing, a new capability of Amazon SageMaker that lets customers easily run the preprocessing, postprocessing and model evaluation workloads on fully managed infrastructure, was announced during re:Invent 2019. 
+
+In this sample, we would lauch a sagemaker processing job in a VPC, the input is videos in S3, and output is inference results (segmentation images) and will be stored in S3.
+
+1. Launch an EC2 instance to play as API server which could be called by sagemaker processing job.
+2. We use pretrained model to do semantic segmentation infenrence from GluonCV model zoo.
+3. Enable Sagemaker Processing vpc mode so it could call API server.
+
+Here is the high level architecture of this sample.
+
+![High level architecture](https://sagemaker-demo-dataset.s3-us-west-2.amazonaws.com/Picture1.png)
+
+
+### GluonCV
+---
+[GluonCV](https://gluon-cv.mxnet.io/) provides implementations of state-of-the-art (SOTA) deep learning algorithms in computer vision. It aims to help engineers, researchers, and students quickly prototype products, validate new ideas and learn computer vision.
+
+[GluonCV model zoo](https://gluon-cv.mxnet.io/model_zoo/index.html) contains six kinds of pretrained model: Classification, Object Detection, Segmentation, Pose Estimation, Action Recognition and Depth Prediction.
+
+In this sample, we will use **deeplab_resnet101_citys** from Segmentation and was trained with **[cityscape dataset](https://www.cityscapes-dataset.com/)**, which focuses on semantic understanding of urban street scenes, so this model is suitable for car view images.
+
+### Prerequisite
+---
+In order to download GPU supported pretrianed model, you need run this sample in **GPU based instance**, such as **ml.p2.xlarge or ml.p3.2xlarge**.
+
+If you only launch none gpu instances in processing jobs, such as c5 type, you could run this demo in none gpu based instances.
+
 
 ## Security
-
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
-
+ 
+ See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+ 
 ## License
-
-This library is licensed under the MIT-0 License. See the LICENSE file.
-
+ 
+ This library is licensed under the MIT-0 License. See the [LICENSE](LICENSE) file.   
---
+++ processing.py
@@ -0,0 +1,131 @@
+# ***************************************************************************************
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.                    *
+#                                                                                       *
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+# software and associated documentation files (the "Software"), to deal in the Software *
+# without restriction, including without limitation the rights to use, copy, modify,    *
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+# permit persons to whom the Software is furnished to do so.                            *
+#                                                                                       *
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION     *
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE        *
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                *
+# ***************************************************************************************
+import argparse
+import os
+import warnings
+import shutil
+import mxnet as mx
+from mxnet import image
+from mxnet.gluon.data.vision import transforms
+from gluoncv.data.transforms.presets.segmentation import test_transform
+import gluoncv
+from gluoncv.utils.viz import get_color_pallete
+import matplotlib.image as mpimg
+import requests
+import cv2
+
+
+def video2frame(video_src_path, video, frames_save_path, frame_width, frame_height, interval):
+    """
+    Extract frame from video by interval
+    :param video_src_path: video src path
+    :param video:　video file name
+    :param frames_save_path:　save path
+    :param frame_width:　frame widty
+    :param frame_height:　frame height
+    :param interval:　interval for frame to extract
+    :return:　frame images
+    """
+    video_name = video[:-4].split('/')[-1]
+    print ("reading video ：", video_name)
+
+    os.makedirs(frames_save_path + video_name, exist_ok=True)
+    each_frame_save_full_path = os.path.join(frames_save_path, video_name) + "/"
+    print('each_frame_save_full_path ' + each_frame_save_full_path)
+    each_video_full_path = os.path.join(video_src_path, video)
+    print('each_video_full_path is ' + each_video_full_path)
+    cap = cv2.VideoCapture(each_video_full_path)
+    frame_index = 0
+    frame_count = 0
+    if cap.isOpened():
+        success = True
+    else:
+        success = False
+        print("Read failed!")
+
+    while(success):
+        success, frame = cap.read()
+
+        if frame_index % interval == 0:
+            print("---> Reading the %d frame:" % frame_index, success)
+            resize_frame = cv2.resize(frame, (frame_width, frame_height), interpolation=cv2.INTER_AREA)
+            # cv2.imwrite(each_video_save_full_path + each_video_name + "_%d.jpg" % frame_index, resize_frame)
+            cv2.imwrite(each_frame_save_full_path + "%d.png" % frame_count, resize_frame,[int( cv2.IMWRITE_JPEG_QUALITY), 95])
+            frame_count += 1
+
+        frame_index += 1
+ 
+    cap.release()
+    return each_frame_save_full_path
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--api_server_address', type=str, default='')
+    args, _ = parser.parse_known_args()
+    
+    print('Received arguments {}'.format(args))
+
+    # Use GPU if available
+    if len(mx.test_utils.list_gpus())!=0:#return range(0, 4)
+        ctx=mx.gpu() #default gpu(0)
+    else:
+        ctx=mx.cpu()
+    
+    model = gluoncv.model_zoo.get_model('deeplab_resnet101_citys', pretrained=True, ctx=ctx) # load the pretrained model trained on Cityscapes dataset
+    
+    input_data_path = '/opt/ml/processing/input_data'
+    output_data_path = '/opt/ml/processing/output_data'
+    print('Reading input data from {}'.format(input_data_path))
+    
+    
+    # Mock call API within VPC, change this link and logic accordingly 
+    api_server_addr = args.api_server_address
+    r=requests.get("http://" + api_server_addr)
+    print(r.text)
+    
+    # Extract frame from videos
+    videos_src_path = input_data_path
+    video_formats = [".mp4", ".avi"]
+    frames_save_path = "/opt/ml/processing/frame_data/"
+    width = 1280
+    height = 720
+    time_interval = 1000
+    
+    video_dirs = os.listdir(input_data_path)
+
+    for video in video_dirs:
+        if video.endswith(('.mp4', '.avi')):
+            frame_save_full_path = video2frame(input_data_path, video, frames_save_path, width, height, time_interval)
+            print('frame_save_full_path is ' + frame_save_full_path)
+            
+            file_dirs = os.listdir(frame_save_full_path)
+            video_name = video[:-4].split('/')[-1]
+
+            # inference by using pretrained model
+            for file in file_dirs:
+                if file.endswith(('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
+                    img = image.imread(os.path.join(frame_save_full_path, file))
+                    img = test_transform(img, ctx)
+                    output = model.predict(img)
+                    predict = mx.nd.squeeze(mx.nd.argmax(output, 1)).asnumpy()
+                    mask = get_color_pallete(predict, 'citys')
+                    mask.save(os.path.join(output_data_path, video_name + '_' + file))
+
+        else:
+            continue
+        
